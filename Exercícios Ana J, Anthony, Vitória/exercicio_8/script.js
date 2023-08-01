50acb3da2e6201d7d93e3a284211e78f48546f1e
vi <108130537+croqueti@users.noreply.github.com>
2023-08-01 11:51:16 -0300

    Update script.js

--- Exercícios Ana J, Anthony, Vitória/exercicio_8/script.js
+++ Exercícios Ana J, Anthony, Vitória/exercicio_8/script.js
@@ -10,29 +10,30 @@ function capturarPrimeiroNumeroInteiro() {
 }
 
 function capturarSegundoNumeroInteiro() {
- const segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
- return segundo_numero_inteiro;
+  const segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
+  return segundo_numero_inteiro;
 }
 
 function capturarTerceiroNumeroInteiro() {
   const terceiro_numero_inteiro = Number(document.getElementById("terceiro_numero_inteiro").value); 
   return terceiro_numero_inteiro;
- }
- function capturarQuartoNumeroInteiro() {
+}
+
+function capturarQuartoNumeroInteiro() {
   const quarto_numero_inteiro = Number(document.getElementById("quarto_numero_inteiro").value); 
   return quarto_numero_inteiro;
- }
-
+}
 
 function somar() {
   let primeiro_numero_inteiro = capturarPrimeiroNumeroInteiro();
   let segundo_numero_inteiro = capturarSegundoNumeroInteiro();
   let terceiro_numero_inteiro = capturarTerceiroNumeroInteiro();
-  let quarto_numero_inteiro = capturarQuartoNumeroInteiro ();
-  let resultado_media = (primeiro_numero_inteiro + segundo_numero_inteiro + terceiro_numero_inteiro + quarto_numero_inteiro)/4;
+  let quarto_numero_inteiro = capturarQuartoNumeroInteiro();
+  let resultado_media = (primeiro_numero_inteiro + segundo_numero_inteiro + terceiro_numero_inteiro + quarto_numero_inteiro) / 4;
+  // Arredondar o resultado para duas casas decimais
+  resultado_media = resultado_media.toFixed(2);
   imprimirResultadoNaTela(resultado_media);
 }
-
 
 function imprimirResultadoNaTela(resultado_media) {
   document.getElementById("resultado_media").value = resultado_media;
